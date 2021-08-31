68f5bc0bad04e69ae954d75b18e9b92a05c40aa6
Edip Çiftçi <ediler1970@gmail.com>
2021-09-01 00:09:20 +0300

    Game window is created

---
+++ Tic Tac Toe Game.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import font as tkFont
+
+def start():
+    
+    root = tk.Tk()
+    root.geometry("600x600")
+    root.resizable(False, False)
+    
+    root.mainloop()
+    
+start()
